8fc510d2720a894f6e821ead5771ac7a7d9a02f2
Bilvish <lavishchauhan78600@gmail.com>
2026-08-22 21:10:47 +0530

    variables

---
+++ 2_variable.py
@@ -0,0 +1,28 @@
+# variable and data types 
+name ="Lavish"   #str
+age = 20         #int
+height = 5.9      #float
+is_student = True #bool  
+
+
+#printing variables
+print (name)
+print (age)
+print (height)
+print (is_student)
+
+
+  #checking data types
+print(type(name))
+print(type (age))
+print(type(height))
+print(type(is_student)) 
+
+
+  # basic opertions with varibles 
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
