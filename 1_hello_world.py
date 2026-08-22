b323d42e23132e02205858417d20a017f96b21e6
Bilvish <lavishchauhan78600@gmail.com>
2026-08-22 20:40:43 +0530

    chapter 1

--- 1_hello_world.py
+++ 1_hello_world.py
@@ -1,1 +1,13 @@
 print("Hello Lavish")
+
+#printing multiple values
+print("my name is bilvish")
+print("i am learning python")
+print("i want to become data engineer")
+
+#printing numbers and calculations
+print(10)
+print(10 +20)
+
+#comments
+print("comments")
