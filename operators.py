1a40edb2378f7e2f0bc8ed2ca5421ba7053026c7
Bilvish <lavishchauhan78600@gmail.com>
2026-08-23 07:56:03 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,23 @@
+a=10
+b=3
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+
+print(a>b)
+print (a==b)
+print(a!=b)
+
+print ( a>5 and b<5)
+print(a>5 or b<5)
+print (not(a>5))
+
+a+=5
+print(a)
+
+a*=2
+print(a)
