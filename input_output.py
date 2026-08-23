b898e37a6a536f53d1bf399ea7a011ad28b4f944
Bilvish <lavishchauhan78600@gmail.com>
2026-08-23 07:38:12 +0530

    msg

---
+++ input_output.py
@@ -0,0 +1,8 @@
+name = input("enter your name:")
+age =int (input("enter your age:"))
+height =float(input("enter your height:"))
+
+print("your name is:", name)
+print("your age is:",age)
+print("your height  is ",height)
+print("your age after 5 years",age + 5)
