f7670a42e076d488bc6445caef28d4204d94ced1
Bilvish <lavishchauhan78600@gmail.com>
2026-08-27 08:17:56 +0530

    string

---
+++ loops.py
@@ -0,0 +1,20 @@
+# for loop
+for i in range(1, 6):
+    print(i)
+
+# while loop
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
+
+# Loop through a string
+name = "Python"
+
+for ch in name:
+    print(ch)
+
+# Even numbers
+for i in range(2, 11, 2):
+    print(i)
