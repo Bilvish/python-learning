9e6e1310b74f01df646ae89b3973e73adb87198f
Bilvish <lavishchauhan78600@gmail.com>
2026-08-28 23:02:15 +0530

    function

---
+++ function.py
@@ -0,0 +1,34 @@
+def greet (name):
+    return "Hello, " + name
+
+print (greet("lavish"))
+print (greet("avish"))
+
+
+def add(a,b):
+    print(a+b)
+add (5,10)
+add(20,30)
+
+
+def add(a, b):
+    return a + b
+
+result = add(10, 20)
+
+print(result)
+
+
+def add (a,b):
+    return a+b  
+x = add (5,10)
+y = add (20,30)
+print (x +y)
+print(y *2) 
+
+
+def square(number):
+    return number ** 2
+
+num = int(input("Enter a number: "))
+print("Square:", square(num))
