7aa518f3d4ef8150de8576aabb12b3b8922c7129
Bilvish <lavishchauhan78600@gmail.com>
2026-09-09 10:50:28 +0530

    feat: practice classes and objects

---
+++ Phase-05-OOP/01-Classes-Objects/classes_objects.py
@@ -0,0 +1,22 @@
+# classes_objects.py
+
+class Product:
+    pass
+
+
+product1 = Product()
+product2 = Product()
+
+
+class Student:
+    def study(self):
+        print("Student is studying")
+
+
+student1 = Student()
+student2 = Student()
+student3 = Student()
+
+student1.study()
+student2.study()
+student3.study()
